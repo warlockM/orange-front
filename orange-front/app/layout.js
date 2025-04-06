70dd47411281926c5da0626359a74d104182fcb2
warlockM <mnkraj529@gmail.com>
2025-04-06 15:58:02 +0530

    Microsoft calrity added

--- orange-front/app/layout.js
+++ orange-front/app/layout.js
@@ -48,6 +48,17 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-22QCNHXG24');
           `}
         </Script>
+        <Script
+  id="clarity-script"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `(function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "qzv0l9e3g5");`,
+  }}
+/>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
