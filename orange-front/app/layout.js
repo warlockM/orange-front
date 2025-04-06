e3389137393aefa509b9aeb1e8dcaf4f2c0abe7a
warlockM <mnkraj529@gmail.com>
2025-04-06 15:12:32 +0530

    GTM fixes

--- orange-front/app/layout.js
+++ orange-front/app/layout.js
@@ -25,23 +25,27 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
        <head>
-        {/* GTM Script in Head */}
-        <Script id="gtm-head" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-K36D982C');`}
+{/* Google Tag Manager script in <head> */}
+<Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-K36D982C');
+          `}
         </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <noscript>
-        
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K36D982C"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-
+  <iframe
+    src="https://www.googletagmanager.com/ns.html?id=GTM-K36D982C"
+    height="0"
+    width="0"
+    style={{ display: 'none', visibility: 'hidden' }}
+  ></iframe>
 </noscript>
         <Navbar />
         {children}
