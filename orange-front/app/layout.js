e617ae4a35669317f898f22611d435cf492764cc
warlockM <mnkraj529@gmail.com>
2025-04-06 15:01:03 +0530

    GTM Added

--- orange-front/app/layout.js
+++ orange-front/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import Script from 'next/script'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,9 +24,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+       <head>
+        {/* GTM Script in Head */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-K36D982C');`}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+        
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K36D982C"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
+</noscript>
         <Navbar />
         {children}
       </body>
